a36d9d82df436dbb70be8367019da649bc169841
septiafia97 <145754125+septiafia97@users.noreply.github.com>
2024-05-01 19:52:05 +0700

    baru

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
-	public function masuk() {
+	public function index() {
 		$this->load->view('template/head');
 		$this->load->view('template/login');
 		$this->load->view('template/foot');
